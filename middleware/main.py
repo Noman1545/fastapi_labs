59bcff405ff0468a5e887d92d90ce96d9f00ed62
noman asim <asimnoman30@gmail.com>
2026-09-04 06:21:51 +0500

    execution time calculation for response header

--- middleware/main.py
+++ middleware/main.py
@@ -6,6 +6,10 @@ app=FastAPI()
 async def log_process_time(request:Request,call_next):
     start_time=time.time()
     response= await call_next(request)
-    process_time=time.time()-start_time
-    print(f"path: {request.url.path} process time is {process_time}")
+    execution_time=time.time()-start_time
+    response.headers["Execution_Time"]=f"{execution_time} sec"
     return response
+
+@app.get("/")
+def home():
+    return{'message':'execution completed'}
