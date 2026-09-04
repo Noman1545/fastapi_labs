d2f7b2c50e4145fd97134c878d586daf359074bf
noman asim <asimnoman30@gmail.com>
2026-09-04 06:32:31 +0500

    request logging middleware

--- middleware/main.py
+++ middleware/main.py
@@ -2,13 +2,20 @@ from fastapi import FastAPI,Request
 import time
 app=FastAPI()
 
+# @app.middleware("http")
+# async def log_process_time(request:Request,call_next):
+#     start_time=time.time()
+#     response= await call_next(request)
+#     execution_time=time.time()-start_time
+#     response.headers["Execution_Time"]=f"{execution_time} sec"
+#     return response
+
 @app.middleware("http")
-async def log_process_time(request:Request,call_next):
-    start_time=time.time()
-    response= await call_next(request)
-    execution_time=time.time()-start_time
-    response.headers["Execution_Time"]=f"{execution_time} sec"
-    return response
+async def log_request(request:Request,call_next):
+    print(f"[Incoming] Methods : {request.method} | Path {request.url.path}")
+    response = await call_next(request)
+    print(f"[Outgoing] Status code : {response.status_code}")
+    return response  
 
 @app.get("/")
 def home():
