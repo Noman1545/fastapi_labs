c426197e4b4ad5c18d6482df0226d9538b17873a
noman asim <asimnoman30@gmail.com>
2026-09-05 03:18:11 +0500

    2 db file practice by using post api

--- database/main.py
+++ database/main.py
@@ -1,31 +1,31 @@
 from fastapi import Depends, FastAPI 
 from sqlalchemy import create_engine,String,Integer,Column
 from sqlalchemy.orm import sessionmaker,declarative_base,Session
+
 app=FastAPI()
-Database_url="sqlite:///./sample.db"
-engine=create_engine(
-    Database_url,
-    connect_args={"check_same_thread":False}
-)
-SessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
+
+Database_url="sqlite:///./new-sample.db"
+
+engine =create_engine(Database_url,connect_args={"check_same_thread":False})
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
 
 class Student(Base):
-    __tablename__= "student-data"
-    ID =Column(Integer,primary_key=True,index=True)
+    __tablename__='student-data'
+    ID=Column(Integer,primary_key=True,index=True)
     Name=Column(String)
+    Age=Column(Integer)
 Base.metadata.create_all(bind=engine)
-def show_db():
-    db=SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.post("/student")
-def insert_data(Name:str , db: Session= Depends(show_db)):
-    student1=Student(Name=Name)
-    db.add(student1)
+def show():
+        db=SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+@app.post("/")
+def insert_data(Name:str,Age:int,db:Session=Depends(show)):
+    s1=Student(Name=Name,Age=Age)
+    db.add(s1)
     db.commit()
-    db.refresh(student1)
-    return {"message":student1}
+    db.refresh(s1)
+    return {"message":s1}
