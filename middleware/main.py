30bf3ac4d46fc41cd8c24da62925ba11cdfc7539
noman asim <asimnoman30@gmail.com>
2026-09-04 06:05:08 +0500

    process time calculate

--- middleware/main.py
+++ middleware/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI,Request
+import time
 app=FastAPI()
 
 @app.middleware("http")
-async def my_middleware(request:Request,call_next):
-    print('response call')
+async def log_process_time(request:Request,call_next):
+    start_time=time.time()
     response= await call_next(request)
-    print('response sent')
+    process_time=time.time()-start_time
+    print(f"path: {request.url.path} process time is {process_time}")
     return response
