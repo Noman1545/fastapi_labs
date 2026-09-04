bcb4669548c4f6551c8ecd9d044d5446a69c9724
noman asim <asimnoman30@gmail.com>
2026-09-05 02:41:58 +0500

    create student data by post api

--- database/main.py
+++ database/main.py
@@ -1,9 +1,7 @@
 from fastapi import Depends, FastAPI 
 from sqlalchemy import create_engine,String,Integer,Column
 from sqlalchemy.orm import sessionmaker,declarative_base,Session
-
 app=FastAPI()
-
 Database_url="sqlite:///./sample.db"
 engine=create_engine(
     Database_url,
@@ -14,10 +12,8 @@ Base=declarative_base()
 
 class Student(Base):
     __tablename__= "student-data"
-
     ID =Column(Integer,primary_key=True,index=True)
     Name=Column(String)
-    
 Base.metadata.create_all(bind=engine)
 def show_db():
     db=SessionLocal()
@@ -25,7 +21,11 @@ def show_db():
         yield db
     finally:
         db.close()
-@app.get("/home")
-def home(db:Session=Depends(show_db)):
-    return {'message':'database created'}
-    
+
+@app.post("/student")
+def insert_data(Name:str , db: Session= Depends(show_db)):
+    student1=Student(Name=Name)
+    db.add(student1)
+    db.commit()
+    db.refresh(student1)
+    return {"message":student1}
