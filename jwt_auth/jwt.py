cdeefee85f251444c3e2b6068392e5cfb0413f87
noman asim <asimnoman30@gmail.com>
2026-09-07 06:35:36 +0500

    oauth2 setup

---
+++ jwt_auth/jwt.py
@@ -0,0 +1,35 @@
+from jose import jwt,JWTError
+from fastapi import FastAPI,HTTPException,Header,Depends
+from datetime import datetime,timedelta,timezone
+app=FastAPI()
+
+SecretKey='noman1125'
+Algorithm="HS512"
+
+def create_token(data:dict):
+    to_encode=data.copy()
+    expiry=datetime.now(timezone.utc) + timedelta( minutes=30)
+    to_encode.update({"exp":expiry})
+    token=jwt.encode(to_encode,SecretKey,algorithm=Algorithm)
+    return token
+
+def verify_token(token:str=Header(None)):
+    if token is None:
+        raise HTTPException(status_code=401,detail="token is missing")
+    try:
+        payload=jwt.decode(token,SecretKey,algorithms=[Algorithm])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=404,detail="invalid token")
+    
+@app.post("/")
+def login(username:str,password:str):
+    if username !='noman' or password !="noman1125":
+        raise HTTPException(status_code=404,detail="invalid")
+    token =create_token({"sub":username})
+    return {"access":token}
+@app.get("/")
+def authorized_user(token:dict=Depends(verify_token)):
+    return {"message":"verified user"}
+    
+    
--- jwt_auth/main.py
+++ jwt_auth/main.py
@@ -1,35 +1,53 @@
+from fastapi import FastAPI,HTTPException,Depends
 from jose import jwt,JWTError
-from fastapi import FastAPI,HTTPException,Header,Depends
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
 from datetime import datetime,timedelta,timezone
+from passlib.context import CryptContext
 app=FastAPI()
-
-SecretKey='noman1125'
-Algorithm="HS512"
-
+SECRETKEY='noman1125'
+ALGORITHM='HS256'
+ACCESS_TOKEN_EXPIRY=30
+# password hashing adding extra strings
+pwd_context=CryptContext(schemes=["bycrypt"],deprecated="auto")
+# oauth setup
+oauth2_scheme=OAuth2PasswordBearer(tokenUrl="login")
+fake_user_db={
+    "admin":{
+        "username":"admin",
+        "hashed_password":pwd_context.hash("1125")
+    }
+}
+def hash_pwd(password:str):
+    return pwd_context.hash(password)
+def verify_pwd(plain_password,hashed_password):
+    return pwd_context.verify(plain_password,hashed_password)
 def create_token(data:dict):
     to_encode=data.copy()
-    expiry=datetime.now(timezone.utc) + timedelta( minutes=30)
-    to_encode.update({"exp":expiry})
-    token=jwt.encode(to_encode,SecretKey,algorithm=Algorithm)
+    exp=datetime.now(timezone.utc)+timedelta(minutes=ACCESS_TOKEN_EXPIRY)
+    to_encode.update({"exp":exp})
+    token=jwt.encode(to_encode,SECRETKEY,algorithm=ALGORITHM)
     return token
 
-def verify_token(token:str=Header(None)):
-    if token is None:
-        raise HTTPException(status_code=401,detail="token is missing")
+@app.post("/login")
+def login(form_data:OAuth2PasswordRequestForm=Depends()):
+    user=fake_user_db.get(form_data.username)
+    if not user or not verify_pwd(form_data.password,user["hashed_password"]):
+        raise HTTPException(status_code=400,detail="invalid username or password")
+    access_token=create_token({"sub":form_data.username})
+    return {"access_token":access_token,"token_type":"bearer"}
+def verify_token(token:str=Depends(oauth2_scheme)):
     try:
-        payload=jwt.decode(token,SecretKey,algorithms=[Algorithm])
-        return payload
-    except JWTError:
-        raise HTTPException(status_code=404,detail="invalid token")
+        payload=jwt.decode(token,SECRETKEY,algorithms=[ALGORITHM])
+        username=payload.get("sub")
     
-@app.post("/")
-def login(username:str,password:str):
-    if username !='noman' or password !="noman1125":
-        raise HTTPException(status_code=404,detail="invalid")
-    token =create_token({"sub":username})
-    return {"access":token}
-@app.get("/")
-def authorized_user(token:dict=Depends(verify_token)):
-    return {"message":"verified user"}
+        if username is None:
+            raise HTTPException(status_code=401,detail="invalid token")
+        return username
+    except jwt.JWTError:
+        raise HTTPException(status_code=401,detail="invalid")
+
+@app.get("/protected")
+def protected_route(username:str=Depends(verify_token)):
+    return username
     
-    
+        
